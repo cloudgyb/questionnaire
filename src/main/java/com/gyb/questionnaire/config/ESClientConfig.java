fa52543d9cf43ba6996947230b825487562998b2
cloudgyb <gyb_cloud@163.com>
2021-03-06 12:17:25 +0800

    refactor: 支持在配置文件中设置elasticsearch的地址

--- src/main/java/com/gyb/questionnaire/config/ESClientConfig.java
+++ src/main/java/com/gyb/questionnaire/config/ESClientConfig.java
@@ -1,6 +1,8 @@
 package com.gyb.questionnaire.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -13,12 +15,14 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
  */
 @Configuration
 public class ESClientConfig extends AbstractElasticsearchConfiguration {
+    @Value("${spring.elasticsearch.address:localhost:9200}")
+    private String esAddress;
 
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
         final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(esAddress)
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
