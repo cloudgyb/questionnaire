910d881d98969446f3328de7e1d1aab39a68d5cd
cloudgyb <gyb_cloud@163.com>
2021-07-30 21:57:55 +0800

    feat: 增强系统安全性
    1.防iframe嵌套，避免“点击劫持”攻击
    2.禁止浏览器对响应资源的嗅探行为

---
+++ src/main/java/com/gyb/questionnaire/config/WebSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.gyb.questionnaire.config;
+
+import com.gyb.questionnaire.config.filters.ContentTypeNoSniffFilter;
+import com.gyb.questionnaire.config.filters.IFrameFilter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Web安全配置类
+ * 通过添加一些过滤器，对每个请求添加一些安全相关的响应头或其他操作以增加安全性
+ *
+ * @author cloudgyb
+ * @since 2021/7/30 21:21
+ */
+@Configuration(proxyBeanMethods = false)
+public class WebSecurityConfig {
+
+    @Bean
+    public ContentTypeNoSniffFilter contentTypeNoSniffFilter() {
+        return new ContentTypeNoSniffFilter();
+    }
+
+    @Bean
+    public IFrameFilter iFrameFilter() {
+        return new IFrameFilter();
+    }
+
+}
